5746ba4e72b6f4ca30142f1ded92bbf93be4d875
Fluorine <f@utu.re>
2022-06-20 12:00:40 -0700

    clang-format

--- 3d.java
+++ 3d.java
@@ -1,62 +1,60 @@
 package calumny;
-import toxi.geom.mesh.*;
-import toxi.geom.*;
-import toxi.volume.*;
 import java.io.IOException;
 import java.nio.ByteBuffer;
+import toxi.geom.*;
+import toxi.geom.mesh.*;
+import toxi.volume.*;
 
+public final class
+3d {
+  public final VolumetricSpaceArray vs;
+  public setup(int returnx, int returny, int z) {
+    vs = new VolumetricSpaceArray(new Vec3D(1, 1, 1), returnx, returny, z);
+  }
+  public final LaplacianSmooth sm = new LaplacianSmooth();
+  {
+    try {
+      dupmatrix(this.vs.getData(), 20);
+      // this.vs.closeSides();
+      // System.err.println("lastresultdata:"+board[4][1][1]);
+      ArrayIsoSurface iso = new ArrayIsoSurface(this.vs);
+      WETriangleMesh we = new WETriangleMesh();
+      // iso.computeSurfaceMesh(null, 0.5f);
+      WETriangleMesh we2 = (WETriangleMesh)iso.computeSurfaceMesh(we, 0.5f);
+      // we2.computeVertexNormals();
+      sm.filter(we2, 1);
+      // we2.saveAsOBJ("/tmp/c.obj");
+      // if(true)
+      //{
+      float[] f;
+      if (!normals)
+        f = we2.getMeshAsVertexArray(null, 0, 3);
+      else {
+        // we2.computeVertexNormals();
+        f = we2.getVertexNormalsAsArray(we2.getMeshAsVertexArray(null, 0, 6), 3,
+                                        6);
+      }
 
-public final class 3d {
-    public final VolumetricSpaceArray vs;
-    public setup(int returnx, int returny, int z) {
-	vs=new VolumetricSpaceArray(new Vec3D(1,1,1),returnx,returny,z); 
+      java.io.DataOutputStream d = new java.io.DataOutputStream(stream);
+      // System.err.print("First three floats are"+f[0]+","+f[1]+","+f[2]);
+      for (int i = 0; i < f.length; i++) {
+        // System.err.print((int) ((f[i]+0.5f)*256.0f));
+        if ((i % 6) > 2)
+          d.write((int)((f[i] + 1.0f) * 128.0f));
+        // else
+        // if(Math.abs(f[i])>0.5) System.err.println("Error"+f[i]);
+        else
+          d.write((int)((f[i] + 0.5f) * 256.0f));
+      }
+      //} else {
+      //	we2.saveAsOBJ(stream);
+
+      //}
+    } catch (IOException e) {
+      e.printStackTrace();
     }
-    public final LaplacianSmooth sm=new LaplacianSmooth();
-    {
-    	try {
-    		dupmatrix(this.vs.getData(),20);
-    		//this.vs.closeSides();
-    	    //System.err.println("lastresultdata:"+board[4][1][1]);
-    		ArrayIsoSurface iso=new ArrayIsoSurface(this.vs);
-    		WETriangleMesh we=new WETriangleMesh();
-    		//iso.computeSurfaceMesh(null, 0.5f);
-    		WETriangleMesh we2=(WETriangleMesh) iso.computeSurfaceMesh(we,0.5f);
-    		//we2.computeVertexNormals();
-    		sm.filter(we2,1);
-    		//we2.saveAsOBJ("/tmp/c.obj");
-    		//if(true)
-    		//{
-    		float[] f;
-    		if(!normals)    		
-    			f=we2.getMeshAsVertexArray(null,0,3);
-    		else
-    		{
-    			//we2.computeVertexNormals();
-    			f=we2.getVertexNormalsAsArray(we2.getMeshAsVertexArray(null,0,6),3,6);
-    		}
-
-    		java.io.DataOutputStream d=new java.io.DataOutputStream(stream);
-    		//System.err.print("First three floats are"+f[0]+","+f[1]+","+f[2]);
-    		for(int i=0;i<f.length;i++)
-    		{
-    			//System.err.print((int) ((f[i]+0.5f)*256.0f));
-    			if((i%6)>2)
-    			  d.write( (int) ((f[i]+1.0f)*128.0f));
-    			//else
-    			//if(Math.abs(f[i])>0.5) System.err.println("Error"+f[i]);
-    			else
-    			d.write( (int) ((f[i]+0.5f)*256.0f) );
-    		}
-    		//} else { 
-    		//	we2.saveAsOBJ(stream);
-    			
-    		//}
-    	} catch (IOException e) 
-    	{ 
-    		e.printStackTrace(); 
-    	}
-    	//ByteBuffer g=ByteBuffer.wrap(new byte[f.length*4]);
-    	//g.asFloatBuffer().put(f);
-    	//return g.array();
-    }
+    // ByteBuffer g=ByteBuffer.wrap(new byte[f.length*4]);
+    // g.asFloatBuffer().put(f);
+    // return g.array();
+  }
 }
--- BitsWithProbability.java
+++ BitsWithProbability.java
@@ -1,59 +1,59 @@
 package calumny;
 import java.util.Random;
 public class BitsWithProbability {
-	public BitsWithProbability(float prob, Random rnd) {
-		this.rnd = rnd;
+  public BitsWithProbability(float prob, Random rnd) {
+    this.rnd = rnd;
 
-		if (prob <= 0f) {
-			zero = true;
-			return;
-		}
+    if (prob <= 0f) {
+      zero = true;
+      return;
+    }
 
-		// Decode IEEE float
-		int probBits = Float.floatToIntBits(prob);
-		mantissa = probBits & 0x7FFFFF;
-		exponent = probBits >>> 23;
-		if (exponent > 0) mantissa |= 0x800000;
-		exponent -= 150;
+    // Decode IEEE float
+    int probBits = Float.floatToIntBits(prob);
+    mantissa = probBits & 0x7FFFFF;
+    exponent = probBits >>> 23;
+    if (exponent > 0)
+      mantissa |= 0x800000;
+    exponent -= 150;
 
-		// Convert prob to the form m * 2**e
-		int ntz = Integer.numberOfTrailingZeros(mantissa);
-		mantissa >>= ntz;
-		exponent += ntz;
-	}
+    // Convert prob to the form m * 2**e
+    int ntz = Integer.numberOfTrailingZeros(mantissa);
+    mantissa >>= ntz;
+    exponent += ntz;
+  }
 
-	/** Determine how many random words we need from the system RNG to
-	 *  generate one output word with probability P.
-	 **/
-	public int iterationCount() {
-		return - exponent;
-	}
+  /**
+   *Determine how many random words we need from the system RNG to
+   *  generate one output word with probability P.
+   **/
+  public int iterationCount() { return -exponent; }
 
-	/** Generate a random number with the desired probability */
-	public long nextLong() {
-		if (zero) return 0L;
+  /** Generate a random number with the desired probability */
+  public long nextLong() {
+    if (zero)
+      return 0L;
 
-		long acc = -1L;
-		int shiftReg = mantissa - 1;
-		for (int bit = exponent; bit < 0; ++ bit) {
-			if ((shiftReg & 1) == 0) {
-				acc &= rnd.nextLong();
-			} else {
-				acc |= rnd.nextLong();
-			}
-			shiftReg >>= 1;
-		}
-		return acc;
-	}
+    long acc = -1L;
+    int shiftReg = mantissa - 1;
+    for (int bit = exponent; bit < 0; ++bit) {
+      if ((shiftReg & 1) == 0) {
+        acc &= rnd.nextLong();
+      } else {
+        acc |= rnd.nextLong();
+      }
+      shiftReg >>= 1;
+    }
+    return acc;
+  }
 
-	/** Value of <code>prob</code>, represented as m * 2**e */
-	private int exponent;  
-	private int mantissa;
+  /** Value of <code>prob</code>, represented as m * 2**e */
+  private int exponent;
+  private int mantissa;
 
-	/** Random data source */
-	private final Random rnd;
+  /** Random data source */
+  private final Random rnd;
 
-	/** Zero flag (special case) */
-	private boolean zero;
-
+  /** Zero flag (special case) */
+  private boolean zero;
 }
